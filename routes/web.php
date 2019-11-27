9c3be47e531f3a5895ebcfbcf4978ee5e3d98db4
lucasan764 <cariverp9@gmail.com>
2019-11-27 18:51:41 -0300

    Registro

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('user/','userController@listaUser');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
