81ca7526de5e269872d9b1357eba336f807f32d8
lucasan764 <cariverp9@gmail.com>
2019-11-27 21:07:29 -0300

    login a la mitad

---
+++ app/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+// class User extends Model
+// {
+//     public $table="users";
+//     public $guarded=[];
+//     public function mascotas()
+//     {
+//       return $this->hasMany('App\Mascotas','user_id');
+//     }
+//     public function post()
+//     {
+//       return $this->hasMany('App\Post','user_id');
+//     }
+// }
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/style.css">
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
