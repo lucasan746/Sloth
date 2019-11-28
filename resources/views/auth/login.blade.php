7d6f28e73cde58095e36fda65575880b61b8d8d7
lucasan764 <cariverp9@gmail.com>
2019-11-28 14:51:54 -0300

    imagenes login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+  <section class="cajadelogin" id="@php
+   $id=chr(rand(ord("a"), ord("s")));
+   echo $id;
+  @endphp" >
+    <article class="login">
+      <h1 class="titulog2">Sloth</h1>
+
+      <form class="iniciases" action="{{ route('login') }}" method="post">
+        @csrf
+        {{-- usuario --}}
+        <label class="errorlogin1" for="email"{{ __('E-Mail Address') }}></label>
+        <input class="inputusua form-control @error('email') is-invalid @enderror"type="email" name="email" placeholder="Nombre de usuario" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+
+        {{-- contraseña --}}
+        <label class="errorlogin2" for="Contraseña"></label>
+        <input class="inputcontra @error('password') is-invalid @enderror" type="password" name="password" placeholder="Contraseña" required autocomplete="current-password">
+        @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+        @enderror
+
+        <br>
+
+        <label class="recuerda" for="recordarus">Recordar Usuario</label>
+        <input class="inputrecuerda"type="checkbox" name="remenber" {{ old('remember') ? 'checked' : '' }}>
+
+        <button type="submit" name="enviar" class="botonlog">Iniciar Sesión</button>
+      </form>
+    </article>
+  </section>
+@endsection
