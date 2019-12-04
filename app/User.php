73d3f1ce8e1d81924fe1a5540b887954e9d35f31
lucasan764 <cariverp9@gmail.com>
2019-12-03 21:49:20 -0300

    Arreglo del merge

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -37,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function post()
+    {
+      return $this->hasMany(Post::class);
+    }
 }
